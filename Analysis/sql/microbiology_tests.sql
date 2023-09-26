8329d594a45317fcdf4d6f27e2093a2815839506
claudia1501 <claujv164@gmail.com>
2023-09-26 15:28:56 +0200

    Table 1

--- Analysis/sql/microbiology_tests.sql
+++ Analysis/sql/microbiology_tests.sql
@@ -1,2 +1,4 @@
+-- CRITERIOS DE INCLUSIÓN: 48 HORAS ANTES Y DESPUES DE PRUEBAS, POSITIVAS
 SELECT *
 FROM `physionet-data.eicu_crd.microlab`
+WHERE (culturetakenoffset <= 2880 AND culturetakenoffset >= -2880)
--- Analysis/sql/patients_date.sql
+++ Analysis/sql/patients_date.sql
@@ -1,4 +1,17 @@
--- FILTRAR
-SELECT *
-FROM `physionet-data.eicu_crd.patient`
-WHERE ((age IS NULL OR TRIM(age) = '') OR age >= '16') AND hospitaladmitsource = 'Emergency Department' AND apacheadmissiondx LIKE '%Sepsis%'
+-- CRITERIOS DE INCLUSIÓN: >16 AÑOS, ENTRADA EMERGENCIA, Y SEPSIS
+SELECT
+  *
+FROM
+  `physionet-data.eicu_crd.patient`
+WHERE
+  (age IS NULL
+    OR TRIM(age) = ''
+    OR age >= '16')
+  AND hospitaladmitsource = 'Emergency Department'
+  AND (apacheadmissiondx LIKE '%Sepsis%'
+    OR apacheadmissiondx = 'Cellulitis and localized soft tissue infections, surgery for'
+    OR apacheadmissiondx = 'Abscess/infection-cranial, surgery for'
+    OR apacheadmissiondx = 'Renal infection/abscess'
+    OR apacheadmissiondx = 'Cellulitis and localized soft tissue infections'
+    OR apacheadmissiondx = 'Thoracotomy for thoracic/respiratory infection'
+    OR apacheadmissiondx = 'Infection/abscess, other surgery for')
