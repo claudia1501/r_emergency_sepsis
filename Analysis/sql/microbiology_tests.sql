5e025fa27affbf07884f0bdcdfa39cdc865f9d6b
claudia1501 <claujv164@gmail.com>
2023-10-16 09:00:22 +0200

    2º CHANGE MICROLAB VARIABLE

--- Analysis/sql/microbiology_tests.sql
+++ Analysis/sql/microbiology_tests.sql
@@ -1,11 +1,21 @@
 -- CRITERIOS DE INCLUSIÓN: 48 HORAS ANTES Y DESPUES DE PRUEBAS, POSITIVAS (RESISTANT)
-SELECT
-  patientunitstayid,
-  MAX(CASE WHEN culturetakenoffset BETWEEN -2880 AND 2880 AND sensitivitylevel = 'Resistant' THEN culturetakenoffset ELSE NULL END) AS culturetakenoffset,
-  MAX(CASE WHEN culturetakenoffset BETWEEN -2880 AND 2880 AND sensitivitylevel = 'Resistant' THEN sensitivitylevel ELSE NULL END) AS sensitivitylevel
+WITH FilteredMicrolab AS (
+  SELECT
+    patientunitstayid,
+    culturetakenoffset,
+    sensitivitylevel,
+    culturesite,
+    organism,
+    antibiotic
+  FROM
+    `physionet-data.eicu_crd.microlab`
+  WHERE
+    culturetakenoffset BETWEEN -2880 AND 2880
+    AND sensitivitylevel = 'Resistant'
+)
+SELECT m.*, fm.culturetakenoffset, fm.sensitivitylevel, fm.culturesite, fm.organism, fm.antibiotic
 FROM
-  `physionet-data.eicu_crd.microlab`
-GROUP BY
-  patientunitstayid
-ORDER BY
-  patientunitstayid ASC;
+  (SELECT DISTINCT patientunitstayid FROM FilteredMicrolab) m
+JOIN FilteredMicrolab fm
+ON m.patientunitstayid = fm.patientunitstayid
+ORDER BY m.patientunitstayid;
