142e4a2e0138580de2c4305cc94dc15ad2606ad8
claudia1501 <132444794+claudia1501@users.noreply.github.com>
2023-11-10 12:07:13 +0100

    Update microbiology_tests.sql

--- Analysis/sql/microbiology_tests.sql
+++ Analysis/sql/microbiology_tests.sql
@@ -3,12 +3,8 @@ SELECT
   patientunitstayid,
   sensitivitylevel,
   CASE 
-    WHEN sensitivitylevel IS NOT NULL AND TRIM(sensitivitylevel) <> '' THEN
-      CASE 
-        WHEN sensitivitylevel = 'Resistant' THEN '1'
-        ELSE '0'
-      END
-    ELSE sensitivitylevel
+    WHEN sensitivitylevel = 'Resistant' THEN '1'
+    WHEN sensitivitylevel = '' THEN ''  -- Para mantener los valores vacíos
+    ELSE '0'
   END AS sensitivity_counf
-FROM `physionet-data.eicu_crd.microlab`
-GROUP BY patientunitstayid, sensitivitylevel;
+FROM `physionet-data.eicu_crd.microlab`;
