7effe7883f38ae06dd6994e33876a0de8e385bfc
claudia1501 <132444794+claudia1501@users.noreply.github.com>
2023-11-14 12:48:50 +0100

    Update sensitivitylevel.sql

--- Analysis/sql/sensitivitylevel.sql
+++ Analysis/sql/sensitivitylevel.sql
@@ -1,9 +1,13 @@
 SELECT 
   patientunitstayid,
-  CASE 
-    WHEN sensitivitylevel = 'Resistant' THEN 'RESISTANT'
-    WHEN sensitivitylevel IN ('Sensitive', 'Intermediate') THEN 'NO RESISTANT'
-    WHEN sensitivitylevel = '' THEN 'NO INFECTION'
-    ELSE sensitivitylevel
-  END AS sensitivity_level
-FROM `physionet-data.eicu_crd.microlab`;
+  MAX(
+    CASE 
+      WHEN sensitivitylevel = 'Resistant' THEN 'Resistant'
+      WHEN sensitivitylevel IN ('Sensitive', 'Intermediate') THEN 'No resistant'
+      WHEN sensitivitylevel = '' THEN 'No infection'
+      ELSE sensitivitylevel
+    END
+  ) AS sensitivity_level
+FROM `physionet-data.eicu_crd.microlab`
+GROUP BY patientunitstayid;
+
