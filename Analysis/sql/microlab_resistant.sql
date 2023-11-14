abef93fa5cdeb96d674c595d74d1cb2d1df505e9
claudia1501 <claujv164@gmail.com>
2023-11-14 11:55:26 +0100

    Changes  inclusion variables.

--- Analysis/sql/microlab_resistant.sql
+++
@@ -1,9 +0,0 @@
--- ONLY RESISTANT
-SELECT 
-  patientunitstayid,
-  'Resistant' AS sensitivity_resistant
-FROM `physionet-data.eicu_crd.microlab` 
-WHERE sensitivitylevel = 'Resistant'
-GROUP BY patientunitstayid, sensitivity_resistant
-ORDER BY patientunitstayid ASC;
-
