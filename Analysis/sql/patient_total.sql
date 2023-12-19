da1c3b1845099649e8ca0a26f0eb4f5a9f75b396
claudia1501 <132444794+claudia1501@users.noreply.github.com>
2023-12-19 12:00:37 +0100

    Update patient_total.sql

--- Analysis/sql/patient_total.sql
+++ Analysis/sql/patient_total.sql
@@ -1,16 +1,4 @@
-WITH pt_age AS (
-  SELECT patientunitstayid, age,
-         CASE WHEN age = '> 89' THEN 90
-         WHEN TRIM(age) = '' THEN NULL
-         ELSE CAST(age AS NUMERIC)
-         END AS age_numeric
-  FROM `physionet-data.eicu_crd.patient`
-)
 SELECT
-  p.patientunitstayid, p.gender, a.age_numeric, p.ethnicity, p.hospitaladmitoffset, p.unittype,unitdischargestatus, 
+  patientunitstayid, gender, age, ethnicity, hospitaladmitoffset, unittype,unitdischargestatus 
 FROM
-  `physionet-data.eicu_crd.patient` p
-JOIN
-  pt_age a
-ON
-  p.patientunitstayid = a.patientunitstayid
+  `physionet-data.eicu_crd.patient` 
