faa616b8e9b3d70175eb036ce71aecbbf4479aa8
claudia1501 <132444794+claudia1501@users.noreply.github.com>
2024-07-09 12:09:13 +0200

    Update shock_vassop.sql
    Change code

--- Analysis/sql/shock_vassop.sql
+++ Analysis/sql/shock_vassop.sql
@@ -1,21 +1,28 @@
 SELECT 
-    patientunitstayid, treatmentstring,
-    MIN(treatmentoffset) AS first_treatmentoffset
-FROM 
-    `physionet-data.eicu_crd.treatment`
-WHERE 
-    treatmentOffset BETWEEN 0 AND 1440
-    AND treatmentstring LIKE 'cardiovascular|shock|vasopressors|vasopressin'
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|phenylephrine (Neosynephrine)'
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|norepinephrine > 0.1 micrograms/kg/min'
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|norepinephrine <= 0.1 micrograms/kg/min'
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|epinephrine > 0.1 micrograms/kg/min'
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|epinephrine <= 0.1 micrograms/kg/min' 
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|dopamine  5-15 micrograms/kg/min' 
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|dopamine >15 micrograms/kg/min' 
-  OR treatmentstring LIKE 'cardiovascular|shock|vasopressors'
-  
-  GROUP BY 
-    patientunitstayid,treatmentstring
+    patientunitstayid,
+    MIN(treatmentoffset) AS first_treatmentoffset,
+    1 AS has_treatment
+FROM (
+    SELECT 
+        patientunitstayid, 
+        treatmentoffset
+    FROM 
+        `physionet-data.eicu_crd.treatment`
+    WHERE 
+        treatmentOffset BETWEEN 0 AND 1440
+        AND (
+            treatmentstring LIKE 'cardiovascular|shock|vasopressors|vasopressin'
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|phenylephrine (Neosynephrine)'
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|norepinephrine > 0.1 micrograms/kg/min'
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|norepinephrine <= 0.1 micrograms/kg/min'
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|epinephrine > 0.1 micrograms/kg/min'
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|epinephrine <= 0.1 micrograms/kg/min' 
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|dopamine 5-15 micrograms/kg/min' 
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors|dopamine >15 micrograms/kg/min' 
+            OR treatmentstring LIKE 'cardiovascular|shock|vasopressors'
+        )
+) AS filtered_treatments
+GROUP BY 
+    patientunitstayid
 
   
